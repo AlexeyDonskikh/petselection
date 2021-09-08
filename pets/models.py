9b8176ff4f56b9c2fd6338c9ae436313799e1443
AlexeyDonskikh <donskikhalexey@gmail.com>
2021-09-08 18:06:37 +0300

    Add pets models

---
+++ pets/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from users.models import User
+
+
+class Species(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Название')
+    slug = models.SlugField(max_length=30, unique=True, verbose_name='url')
+
+    class Meta:
+        verbose_name = 'Разновидность домашнего животного'
+        verbose_name_plural = 'Разновидности домашних животных'
+
+    def __str__(self):
+        return self.name
+
+
+class Breed(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Название')
+    slug = models.SlugField(max_length=30, unique=True, verbose_name='url')
+    species = models.ForeignKey(Species, on_delete=models.SET_NULL,
+                                blank=True, null=True,
+                                verbose_name='Разновидность',
+                                related_name='pets')
+
+    class Meta:
+        verbose_name = 'Порода'
+        verbose_name_plural = 'Породы'
+
+    def __str__(self):
+        return self.name
+
+
+class Pet(models.Model):
+    name = models.TextField(verbose_name='Кличка')
+    age = models.PositiveSmallIntegerField(db_index=True,
+                                           verbose_name='Возраст')
+    species = models.ForeignKey(Species, on_delete=models.SET_NULL,
+                                blank=True, null=True,
+                                verbose_name='Разновидность',
+                                related_name='pets')
+    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL,
+                              blank=True, null=True,
+                              related_name='pets',
+                              verbose_name='Порода')
+    description = models.TextField(blank=True, null=True,
+                                   verbose_name='Описание')
+    master = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,
+                               verbose_name='Владелец', related_name='pets')
+
+    class Meta:
+        verbose_name = 'Питомец'
+        verbose_name_plural = 'Питомец'
+
+    def __str__(self):
+        return self.name
