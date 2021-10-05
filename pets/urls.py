963393575a749938be87468056340b97d1b469ac
AlexeyDonskikh <donskikhalexey@gmail.com>
2021-10-05 12:25:55 +0300

    Create post, my_posts

--- pets/urls.py
+++ pets/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('my_pets/', MyPetListView.as_view(), name='my_pets'),
     path('my_pets/<slug:slug>/', PetDetailView.as_view(), name='pet_detail'),
     path('add/', PetAddView.as_view(), name='pet_add'),
-    path('update/', PetUpdateView.as_view(), name='pet_update'),
+    path('update/<slug:slug>/', PetUpdateView.as_view(), name='pet_update'),
     ]
