63607aaaced1ea357f851ccc5fc3b400e5aced88
AlexeyDonskikh <donskikhalexey@gmail.com>
2021-10-14 11:27:33 +0300

    auth users

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django.contrib.auth.forms import UserCreationForm
 
 from users.models import User
@@ -7,3 +9,14 @@ class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email')
+
+    def clean(self):
+        cleaned_data = super(CreationForm, self).clean()
+        username = cleaned_data.get('username')
+        print(username)
+        if not re.match(r'[a-z_][0-9a-z_]*$', username):
+            self.add_error('username',
+                           'Допустимы только буквы латинского алфавита, '
+                           'цифры и нижнее подчеркивание. Username не может '
+                           'начинаться с цифры')
+        return cleaned_data
